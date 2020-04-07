72cb183b0bf052f3988353886a3c83d77de61ce0
cj <cjrh1films@gmail.com>
2020-04-06 19:45:38 -0700

    Refactor code to be onclick

--- Assets/Scripts/PieceOperator.cs
+++ Assets/Scripts/PieceOperator.cs
@@ -5,36 +5,38 @@ using UnityEngine.EventSystems;
 
 public class PieceOperator : MonoBehaviour
 {
-    public GameObject piece;
-    public LayerMask layermask; //Research LayerMask
-    public float rayLength;
+    public GameObject Piece;
+
+    public void PlacePiece() 
+    {
+        if (Piece != null)
+        {
+            bool isActive = Piece.activeSelf;
+            
+            Piece.SetActive(!isActive);
+        }
+    }
 
     public void Update()
     {
-        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
-        //Research GetMouseButton and EventSystem
-        {
 
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, rayLength, layermask))
-            {
 
-                Instantiate( piece, hit.transform.position, hit.transform.rotation);
-                piece.SetActive(true);
+        // RAYCAST WORKS EVERY FRAME, WHICH WE DON'T WANT
+        // if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+        // //Research GetMouseButton and EventSystem
+        // {
+
+        //     RaycastHit hit;
+        //     Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //     if (Physics.Raycast(ray, out hit, rayLength, layermask))
+        //     {
+
+        //         Instantiate( piece, hit.transform.position, hit.transform.rotation);
+        //         piece.SetActive(true);
 
                 
-                Debug.Log(hit.collider.name);
-            }
+        //         Debug.Log(hit.collider.name);
+        //     }
         }
-        // if(Piece != null)
-        // {
-        //     Piece.SetActive(true);
-        // }
+       
     }
-
-    //useful raycast code
-    // RaycastHit hit;
-    //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-}
