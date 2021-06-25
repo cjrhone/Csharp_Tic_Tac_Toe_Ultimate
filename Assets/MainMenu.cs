05f628ca246e77d8a6ee88d66ca120c284403b14
CJ Rhone <cjrh1films@gmail.com>
2021-06-25 16:11:46 -0700

    fix play menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayGameVsPlayer() // Gameboard against player 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // goes to next index scene ( 0 to 1 )
 
@@ -15,5 +15,11 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void PlayGameVsCPU() // Gameboard against CPU
+    {
+        Debug.Log("Player VS CPU");
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2); // goes scene index 2 ( CPU Gameboard ) 
     }
 }
