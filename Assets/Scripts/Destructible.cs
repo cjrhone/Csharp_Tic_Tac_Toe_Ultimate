c87e845d92b01df92240c3b84e064f3ce91c05a6
CJ Rhone <cjrh1films@gmail.com>
2021-06-29 20:26:32 -0700

    Make a choice

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -1,24 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class Destructible : MonoBehaviour
 {
+    public GameObject intactVersion;
     public GameObject destroyedVersion;
+    public GameObject choiceText;
     public float radius = 5.0f;
     public float force = 10.0f;
+    public bool choice = false;
+
+    // Rigidbody test_rb = destroyedVersion.GetComponent<Rigidbody>();
+    // public GameObject test_rb = destroyedVersion.GetComponent<Rigidbody>();
 
     void OnMouseDown()
     {
+        Debug.Log("Boom");
         Instantiate(destroyedVersion, transform.position, transform.rotation);
-        // Destroy(gameObject);
+
+        destroyedVersion.GetComponent<Rigidbody>().useGravity = false;
+        destroyedVersion.GetComponent<Rigidbody>().AddExplosionForce(force, transform.position, radius);
+        Destroy(intactVersion); // This Instance Dies here.
+
+
+        if(!choice)
+        {
+            bool choice = true;
+            Debug.Log("A Choice has been made True");
+            choiceText.SetActive(true);
+        }
         
         Rigidbody rb = destroyedVersion.AddComponent<Rigidbody>();
         rb.useGravity = false;
         rb.AddExplosionForce(force, transform.position, radius);
 
         Destroy(destroyedVersion, 3f);
-        Destroy(gameObject);
 
 
     }
