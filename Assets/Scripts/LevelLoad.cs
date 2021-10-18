c6571f87d30574694296ed78266196b59b2d52e0
CJ Rhone <cjrh1films@gmail.com>
2021-10-18 02:34:31 -0700

    add loading screen and credits

--- Assets/Scripts/LevelLoad.cs
+++ Assets/Scripts/LevelLoad.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoad : MonoBehaviour
 {
@@ -11,21 +12,30 @@ public class LevelLoad : MonoBehaviour
 
     public bool vsCPU;
 
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
 
-    public void PlayGameVsPlayer() // Gameboard against player 
+
+    // public void PlayGameVsPlayer() // Gameboard against player 
+    // {
+    //     FindObjectOfType<AudioManager>().Play(Sound.SoundType.Select);
+    //     FindObjectOfType<AudioManager>().StopPlaying(Sound.SoundType.Menu);
+    //     StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1)); // passing levelIndex to LoadLevel through GetActiveScene() 
+    //     FindObjectOfType<AudioManager>().Play(Sound.SoundType.Ready_VO);
+
+    // }
+
+    // public void QuitGame()
+    // {
+    //     Debug.Log("Quit");
+    //     Time.timeScale = 0;
+    //     Application.Quit();
+    // }
+
+    public void LoadLevel(int sceneIndex)
     {
-        FindObjectOfType<AudioManager>().Play(Sound.SoundType.Select);
-        FindObjectOfType<AudioManager>().StopPlaying(Sound.SoundType.Menu);
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1)); // passing levelIndex to LoadLevel through GetActiveScene() 
-        FindObjectOfType<AudioManager>().Play(Sound.SoundType.Ready_VO);
-
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("Quit");
-        Time.timeScale = 0;
-        Application.Quit();
+       StartCoroutine(LoadAsynchronously(sceneIndex)); 
     }
 
     public void PlayGameVsCPU() // Gameboard against CPU
@@ -35,15 +45,31 @@ public class LevelLoad : MonoBehaviour
         vsCPU = true;
     }
 
-    IEnumerator LoadLevel(int levelIndex)
+    IEnumerator LoadAsynchronously (int sceneIndex)
     {
-        //Play animation
-        transition.SetTrigger("Start"); // "Start" is the Parameter we named and set in animation controller 
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
-        //Wait
-        yield return new WaitForSeconds(transitionTime); // pauses corountine for x seconds 
+        loadingScreen.SetActive(true);
 
-        //Load scene
-        SceneManager.LoadScene(levelIndex);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f); // Clamps value between 0 and 1 and returns value.
+            Debug.Log(operation.progress);
+
+            slider.value = progress;
+            progressText.text = progress * 100f + "%"; //converts progress integer to 100
+
+            yield return null;
+        }
+
+        
+        // //Play animation
+        // transition.SetTrigger("Start"); // "Start" is the Parameter we named and set in animation controller 
+
+        // //Wait
+        // yield return new WaitForSeconds(transitionTime); // pauses corountine for x seconds 
+
+        // //Load scene
+        // SceneManager.LoadScene(sceneIndex);
     }
 }
